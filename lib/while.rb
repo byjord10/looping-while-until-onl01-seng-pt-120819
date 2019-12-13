43edca061e74df2f8ed2237b4dd52e05651c508e
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-13 20:52:02 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,8 @@
 require_relative
-
+levitation_force = 0
 def using_while
-  levitation_force = 0
   until counter == 10
     puts "Wingardium Leviosa"
     counter += 1
-#your code here
-end
+  end
+end 
